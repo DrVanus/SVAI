ac747087acaadb7d4664240fbe0400efae9ad8c7
DrVanus <danielm188@aol.com>
2025-02-25 03:46:49 -0500

    Delete StoryVault AI 4 directory

--- StoryVault AI 4/RowWidthKey.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct RowWidthKey: PreferenceKey {
-    static let defaultValue: CGFloat = 0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        let next = nextValue()
-        if next > value { value = next }
-    }
-}
